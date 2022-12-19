a9515ad0489dfab9db2f43db41273a5c197f6ab1
felipe <feliperc@ciandt.com>
2022-12-19 12:09:25 -0800

    running version

--- node/CarAPIClient/app.js
+++ node/CarAPIClient/app.js
@@ -1,88 +1,5 @@
 const carApi = require('./carApi')
 
-const asyncApiCall = async () => {
-    const response = await carApi.httpGetCars()
-
-    //console.log(response.data)
-    // var carSpecs = JSON.parse(response.data);
-    let carSpecs = response.data;
-
-    const bestFeatures = [];
-
-    for (let index = 0; index < 50; index++) {
-
-        const featureValues = [];
-
-        carSpecs.forEach(function(table) {
-            var featureValue = table['feature' + index];
-            //console.log(featureValue);
-
-            featureValues.push(featureValue);
-            
-        });
-
-        //console.log(featureValues);
-
-        // apply logic to determine the best
-        if (CompareByAvailability(featureValues)) {
-            //console.log("adding best" + index);
-            bestFeatures.push('feature' + index);
-        }
-    }
-    
-    //console.log(bestFeatures);
-
-    return bestFeatures;
-    
-}
-
-
-
-//return ret
-
-var http = require('http');
-const { readSync } = require('fs');
-
-var app = http.createServer(function(req,res){
-    res.setHeader('Content-Type', 'application/json');
-    console.log('calling...')
-    let bestFeatures;
-    let ret = asyncApiCall().then(function(result) { 
-        bestFeatures = result;
-        console.log(bestFeatures);
-        res.end(JSON.stringify({bestFeatures}));
-    });
-
-    //console.log(d);
-    
-    //res.end(JSON.stringify({ a: 1 }));
-});
-app.listen(3002);
-
-//var app = http.createServer(function(req,res){
-    //res.setHeader('Content-Type', 'application/json');
-    //console.log('calling...')
-    //let ret = asyncApiCall();
-    //res.end(JSON.stringify({ a: 1 }));
-    //res.end(JSON.stringify({ a: 1 }));
-//});
-
-/* app.get('/cars', function (req, res) {   
-    res.contentType('application/json');
-    res.setHeader('Content-Type', 'application/json');
-    console.log('calling...')
-    let ret = asyncApiCall();
-    res.end(JSON.stringify(ret));
-}); */
-
-//app.listen(3000)
-
-/* const requestListener = function(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify(data, null, 3));
-} */
-
 function CompareByAvailability(values) {
 
 	let result = true;
@@ -99,3 +16,50 @@ function CompareByAvailability(values) {
 	return result;
 
 }
+
+const asyncApiCall = async () => {
+    const response = await carApi.httpGetCars()
+
+    let carSpecs = response.data;
+
+    const bestFeatures = [];
+
+    for (let index = 0; index < 50; index++) {
+
+        const featureValues = [];
+
+        carSpecs.forEach(function(table) {
+            var featureValue = table['feature' + index];
+
+            featureValues.push(featureValue);
+            
+        });
+
+        // apply logic to determine the best
+        if (CompareByAvailability(featureValues)) {
+            bestFeatures.push('feature' + index);
+        }
+    }
+    
+    return bestFeatures;
+    
+}
+
+
+var http = require('http');
+const { readSync } = require('fs');
+
+var app = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    console.log('calling...')
+    let bestFeatures;
+    let ret = asyncApiCall().then(function(result) { 
+        bestFeatures = result;
+        console.log(bestFeatures);
+        res.end(JSON.stringify({bestFeatures}));
+    });
+
+});
+app.listen(3002);
+
+
--- node/CarAPIClient/carApi.js
+++ node/CarAPIClient/carApi.js
@@ -3,23 +3,6 @@ const axios = require("axios");
 const BASE_URL = `http://localhost:9000/api`
 
 module.exports = {
-    getCompatibility: (yourName, yourBirthday, theirName, theirBirthday) => axios({
-        method:"POST",
-        url : BASE_URL + `/zodiac_compatibility/result/`,
-        headers: {
-            "content-type":"application/x-www-form-urlencoded",
-            "x-rapidapi-host":"astrology-horoscope.p.rapidapi.com",
-            "x-rapidapi-key": "yourapikey"
-        },
-        params: {
-            mystic_dob: yourBirthday,
-            mystic_dob2: theirBirthday,
-            mystic_name: yourName,
-            mystic_name2: theirName
-        }
-    }),
-
-
     httpGetCars: () => axios({
         method:"GET",
         url : BASE_URL + `/cars/`
